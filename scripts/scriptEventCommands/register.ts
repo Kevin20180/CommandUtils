6c95daa056cd89a951ba49201d42bf2978e4971d
Kevin <kevinlucas7910@gmail.com>
2026-05-20 22:29:34 -0400

    feat: adiciona ScriptEventManager e ScriptEventCommand para gerenciar script events

---
+++ scripts/scriptEventCommands/register.ts
@@ -0,0 +1,90 @@
+import * as mc from "@minecraft/server";
+import EventEmitter from "eventemitter3";
+import { getSourceFromOrigin, isPlayer } from "../utils";
+
+export class ScriptEventManager extends EventEmitter {
+    _scriptEventsById: Map<string, ScriptEventCommand>;
+    
+    constructor() {
+        super();
+        this._scriptEventsById = new Map();
+    }
+
+    register(id: string, onRun?: (event: ScriptCommandEvent) => void): ScriptEventCommand {
+        let scriptEvent = this._scriptEventsById.get(id);
+        if(scriptEvent) return scriptEvent;
+
+        scriptEvent = new ScriptEventCommand(id);
+        this._scriptEventsById.set(id, scriptEvent);
+
+        if(onRun) scriptEvent.on('run', onRun);
+
+        return scriptEvent;
+    }
+
+    getScriptEvent(id: string): ScriptEventCommand | undefined {
+        return this._scriptEventsById.get(id);
+    }
+
+    getScriptEvents(): ScriptEventCommand[] {
+        return [...this._scriptEventsById.values()];
+    }
+}
+
+export class ScriptEventCommand extends EventEmitter<ScriptEventEvents> {
+    readonly id: string;
+
+    constructor(id: string) {
+        super();
+        this.id = id;
+    }
+}
+
+export interface ScriptCommandEvent {
+    message: string,
+    source?: mc.Entity | mc.Block,
+    sourceEntity?: mc.Entity,
+    sourceBlock?: mc.Block,
+    initiator?: mc.Entity,
+    sourceType: mc.ScriptEventSource,
+}
+
+export type ScriptEventEvents = {
+    run: (event: ScriptCommandEvent) => void
+}
+
+export const scriptEventManager = new ScriptEventManager();
+
+mc.system.afterEvents.scriptEventReceive.subscribe((event) => {
+    const { id, message, sourceEntity, sourceBlock, initiator, sourceType } = event;
+
+    const source = getSourceFromOrigin(event);
+
+    const scriptEvent = scriptEventManager.getScriptEvent(id);
+    if(!scriptEvent) {
+        if(!isPlayer(source)) return;
+        return source.sendMessage(`§cScript event '${id}' inexistente.`);
+    }
+
+    scriptEvent.emit("run", {
+        message,
+        source,
+        sourceEntity,
+        sourceBlock,
+        initiator,
+        sourceType
+    })
+})
+
+scriptEventManager.register('cmdutils:help', ({ source }) => {
+    if(!isPlayer(source)) return;
+    const player = source;
+
+    let message = "Script event disponíveis:\n";
+
+    for(let cmd of scriptEventManager.getScriptEvents()) {
+        message += "§a" + cmd.id + "\n";
+    }
+
+    player.sendMessage(message);
+})
