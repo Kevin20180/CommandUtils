113d6ea2e23d00022eb8261eb941fc3df1dc022b
Kevin20180 <kevinlucas7910@gmail.com>
2026-05-21 14:57:48 -0400

    feat: adiciona cmdutils:open_action_menu para abrir menus à jogadores

--- scripts/scriptEventCommands/commands.ts
+++ scripts/scriptEventCommands/commands.ts
@@ -1,6 +1,14 @@
 import * as mc from "@minecraft/server";
 import { scriptEventManager } from ".";
 import { toString } from "../utils";
+import { ActionMenu } from "../menus";
+
+export interface ActionMenuData {
+    title?: string,
+    body?: string,
+    buttons?: string[],
+    on_open?: string[]
+}
 
 scriptEventManager.register('cmdutils:help', (event) => {
     const { sourcePlayer: player } = event;
@@ -24,4 +32,56 @@ scriptEventManager.register("cmdutils:run_script", (event) => {
     } catch(e) {
         player?.sendMessage("§c" + e);
     }
+})
+
+scriptEventManager.register("cmdutils:open_action_menu", (event) => {
+    const { sourcePlayer: player, message } = event;
+    if(!player) return;
+
+    let data: ActionMenuData | undefined;
+    try {
+        data = JSON.parse(message);
+    } catch(e) {
+        return player.sendMessage("§c" + e);
+    }
+    
+    if(!data) return player.sendMessage("§cNenhum dado fornecido.");
+
+    if(data.title != undefined && typeof data.title !== "string") return player.sendMessage("§cPropriedade 'title' deve ser string.");
+    if(data.body != undefined && typeof data.body !== "string") return player.sendMessage("§cPropriedade 'body' deve ser string.");
+    if(data.buttons != undefined && !Array.isArray(data.buttons)) return player.sendMessage("§cPropriedade 'buttons' deve ser string[].");
+    if(data.on_open != undefined && !Array.isArray(data.on_open)) return player.sendMessage("§cPropriedade 'on_open' deve ser string[].");
+
+    const menu = new ActionMenu();
+    if(data.title) menu.title(data.title);
+    if(data.body) menu.body(data.body);
+    
+    if(data.on_open) {
+        for(let i in data.on_open) {
+            let cmd = data.on_open[i]!;
+            if(typeof cmd !== "string") return player.sendMessage(`§cPropriedade 'on_open[${i}]' deve ser string.`);
+        }
+    }
+    
+    if(data.buttons) {
+        for(let i in data.buttons) {
+            let button = data.buttons[i]!;
+            if(typeof button !== "string") return player.sendMessage(`§cPropriedade 'buttons[${i}]' deve ser string.`);
+            
+            menu.button(button, undefined, () => {
+                if(!data.on_open) return;
+                
+                let cmd = data.on_open[i];
+                if(!cmd || !player.isValid) return;
+                
+                try {
+                    player.runCommand(cmd);
+                } catch(e) {
+                    player.sendMessage("§c" + e);
+                }
+            })
+        }
+    }
+    
+    menu.open(player);
 })
